c4568e7e5b3892e018118cd227122cde5da75dcf
linyuan_1991@163.com <ly19910927@foxmail.com>
2020-08-02 22:33:26 +0800

    updates with some changes

---
+++ src/main/java/com/blockchain/manager/controller/SdkJarController.java
@@ -0,0 +1,20 @@
+package com.blockchain.manager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Eric
+ * @create: 2020-08-02 22:28
+ **/
+
+@RestController(value = "/jar")
+public class SdkJarController {
+
+
+    @RequestMapping(value = "/get/latest")
+    public void getLatestController() {
+
+    }
+
+}
